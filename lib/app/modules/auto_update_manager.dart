3b4bf940304de2d40495ceb848e665b32c92c9ce
GooRingX <gooringx@gmail.com>
2025-06-16 18:13:29 +0800

    update

--- lib/app/modules/auto_update_manager.dart
+++ lib/app/modules/auto_update_manager.dart
@@ -295,7 +295,7 @@ class AutoUpdateManager {
           if (item.version.isEmpty || item.url.isEmpty) {
             continue;
           }
-          if (abis.isNotEmpty) {
+          if (item.abis.isNotEmpty) {
             bool hasAbi = false;
             for (var abi in abis) {
               abi = abi.trim();
